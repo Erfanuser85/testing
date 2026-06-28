c7b6afb8d97f6e0c3189e28e497d7cdc85aad251
Erfanss1405 <141412738+Erfanuser85@users.noreply.github.com>
2026-06-28 19:36:02 +0330

    add adadAval code

---
+++ adadaval2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int checkprime(int n){
+    if(n==1){
+        return 0;
+    }
+    if(n==2){
+        return 1;
+    }
+    for(int i=2;i*i<=n;i++){
+        while(n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+void printprime(int a, int b){
+    int first = 1;               
+    for (int i = a + 1; i < b; ++i){
+        if (checkprime(i) !=0){
+            if (first!=1) printf(","); 
+            printf("%d", i);
+            first = 0;               
+        }
+    }
+}
+int main(){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printprime(a,b);
+    return 0;
+}
